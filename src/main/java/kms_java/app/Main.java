452a41f86583e7f2b44dc23000105525d621d3e1
MPC <mpc1945@gmail.com>
2026-06-24 00:45:33 +0700

    feat: setup struktur project MVC dan package

---
+++ src/main/java/kms_java/app/Main.java
@@ -0,0 +1,11 @@
+package kms_java.app;
+
+/**
+ * Entry point aplikasi KMS Putusan Pengadilan Narkotika.
+ * @author [Nama Kamu]
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("KMS Putusan Pengadilan Narkotika - Starting...");
+    }
+}
